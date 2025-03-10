465f39f0cc4e6f1b5347ae29a19b2a6aaa8355e6
mohdswar9@gmail.com <mohdswar9@gmail.com>
2025-03-10 13:08:27 +0300

    thats my branch

--- exercises.py
+++ exercises.py
@@ -15,3 +15,63 @@ def calculate_area_triangle( base, height):
     return area
 
 print('Exercise 1:', calculate_area_triangle(10, 5))
+
+# Exercise 2: Calculate Simple Interest
+#
+# Write a function named `simple_interest` that takes principal, rate of interest (as a percentage), and time (years).
+# Calculate and return the simple interest using the formula (principal * rate * time) / 100.
+#
+# Examples:
+# simple_interest(1000, 5, 2) should return 100.
+# simple_interest(1500, 3.5, 5) should return 262.5.
+#
+# Define your function and call it to see the result.
+def simple_interest(principal, rate, time):
+    intrest=(principal * rate * time) / 100
+    return intrest
+
+
+
+print('Exercise 2:', simple_interest(1000, 5, 2))
+
+# Exercise 3: Apply a Discount
+#
+# Write a function named `apply_discount` that takes a product's price and a discount percentage (from 0 to 100).
+# Return the new price after applying the discount.
+#
+# Examples:
+# apply_discount(100, 25) should return 75.
+# apply_discount(80, 10) should return 72.
+#
+# Define your function and call it to display the discounted price.
+
+def apply_discount(price, discount):
+    new_price= price - (price*(discount/100))
+    return new_price
+
+print('Exercise 3:', apply_discount(100, 25))
+
+# Exercise 4: Convert Temperature
+#
+# Write a function called `convert_temperature` that takes a
+# temperature and a unit ('C' for Celsius, 'F' for Fahrenheit)
+# and converts the temperature to the other unit.
+# The formula for converting Celsius to Fahrenheit is (Celsius * 9/5) + 32.
+# The formula for converting Fahrenheit to Celsius is (Fahrenheit - 32) * 5/9.
+#
+# Examples:
+# convert_temperature(0, 'C') should return 32.0.
+# convert_temperature(32, 'F') should return 0.0.
+#
+# Define the function and then call it below.
+
+def convert_temperature(temperature, unit):
+    if unit == 'C':
+        return (temperature * 9/5) + 32
+    elif unit == 'F':
+        return (temperature - 32) * 5/9
+
+
+
+print('Exercise 4: Convert 0°C to Fahrenheit:', convert_temperature(0, 'C'))
+print('Exercise 4: Convert 32°F to Celsius:', convert_temperature(32, 'F'))
